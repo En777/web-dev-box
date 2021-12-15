1b92a65f04cd530d65baa1979d000aa54e8b51bc
En <1@mail>
2021-12-15 13:05:26 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="App">
+    <div>Hello world</div>
+  </div>
+</template>
+
+<script>
+// import Demo from './Demo'
+
+export default {
+  components: {
+    // Demo
+  },
+  // data () {
+  //   return {
+
+  //   }
+  // },
+  // methods: {
+
+  // },
+  // mounted () {
+
+  // }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/renderer/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App'
+// import { shell } from 'electron'
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = 'true'
+
+// Vue.use(ElementUI)
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
